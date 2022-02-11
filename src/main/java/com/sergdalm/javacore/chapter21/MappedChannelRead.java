1ae836f7bc995d7bc5fa6e97e3b5476284271e09
sergdalm <30310397+sergdalm@users.noreply.github.com>
2022-02-11 18:36:07 +0300

    Chapter 21: NIO (using mapped channel to read a file, using NIO to write a file)

---
+++ src/main/java/com/sergdalm/javacore/chapter21/MappedChannelRead.java
@@ -0,0 +1,33 @@
+package src.main.java.com.sergdalm.javacore.chapter21;
+
+// Use a mapped channel to read a file. Requires JDK 7 or later.
+
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.nio.file.*;
+
+public class MappedChannelRead {
+    public static void main(String[] args) {
+
+        // Obtain a channel to file within a try-with-resources block.
+        try(FileChannel fChan = (FileChannel) Files.newByteChannel(Paths.get("resources/text.txt"))) {
+
+            // Get the size of the file.
+            long fSize = fChan.size();
+
+            // Now, map the file into a buffer.
+            MappedByteBuffer mBuf = fChan.map(FileChannel.MapMode.READ_ONLY, 0, fSize);
+
+            // Read and display bytes from buffer.
+            for(int  i = 0; i < fSize; i++)
+                System.out.print((char) mBuf.get());
+
+            System.out.println();
+        } catch(InvalidPathException e) {
+            System.out.println("Path Error " + e);
+        } catch (IOException e) {
+            System.out.println("I/O Error " + e);
+        }
+    }
+}
